504b3ed26f73905dd02077b75858056fb10c4d00
Forato <lucas7forato@gmail.com>
2024-08-05 18:10:39 -0300

    feat: made nested lists possible

---
+++ internal/utils/ast.go
@@ -0,0 +1,69 @@
+package utils
+
+import (
+	sitter "github.com/smacker/go-tree-sitter"
+)
+
+func HasListChild(node *sitter.Node) bool {
+	for {
+		if node.NextSibling() == nil {
+			break
+		}
+		if node.NextSibling().Type() == "list" {
+			return true
+		}
+		node = node.NextSibling()
+	}
+	return false
+}
+
+func GetListChild(node *sitter.Node) *sitter.Node {
+	for {
+		if node.NextSibling() == nil {
+			break
+		}
+		if node.NextSibling().Type() == "list" {
+			return node.NextSibling()
+		}
+		node = node.NextSibling()
+	}
+	return nil
+}
+
+func IsOrderedList(node *sitter.Node) bool {
+	if node.Type() != "list" || node.ChildCount() == 0 {
+		return false
+	}
+
+	if node.Child(0).Type() == "list_item" {
+		node = node.Child(0)
+	}
+
+	if node.ChildCount() == 0 {
+		return false
+	}
+
+	if node.Child(0).Type() == "list_marker_parenthesis" || node.Child(0).Type() == "list_marker_dot" {
+		return true
+	}
+
+	return false
+}
+
+func HasNestedList(node *sitter.Node) bool {
+  for i := 0; i < int(node.ChildCount()); i++ {
+    if node.Child(i).Type() == "list" {
+      return true
+    }
+  }
+  return false
+}
+
+func GetNestedListNode(node *sitter.Node) *sitter.Node {
+  for i := 0; i < int(node.ChildCount()); i++ {
+    if node.Child(i).Type() == "list" {
+      return node.Child(i)
+    }
+  }
+  return nil
+}
