57fa9074e55c5c2cc3b1c81a6a58d7d86487f949
Forato <lucas.e.forato@gmail.com>
2024-08-23 20:11:53 -0300

    feat: I am so happy that this is finally working, life if beautiful

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,15 @@
+
 document.addEventListener("htmx:afterRequest", function(event) {
-  document.querySelector(".sidebar-btn").forEach(btn => {
+  document.querySelectorAll(".sidebar-btn").forEach(btn => {
     btn.classList.add("text-white");
     btn.classList.remove("text-orange-400");
-  })
+  });
 
-  const btn = event.detail.elt
-  btn.classList.remove("text-white")
-  btn.classList.add("text-orange-400")
+  const btn = event.detail.elt;
 
+  if (btn && btn.classList.contains("sidebar-btn")) {
+    btn.classList.remove("text-white");
+    btn.classList.add("text-orange-400");
+  }
 });
+
